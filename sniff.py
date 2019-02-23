ded89738f387d27090782cd9c041270cfb61ec3c
Borab3 <nadivge@gmail.com>
2019-02-23 15:23:08 -0700

    Update sniff.py

--- sniff.py
+++ sniff.py
@@ -5,4 +5,4 @@ import binascii
 s=socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800)) #socket class
 
 for x in range(1,10):
-    print s.recvfrom(65565)
+    print(s.recvfrom(65565))
