19af8b2c635b16d64fb41f40ae9b7d49c3a8d686
Borab3 <nadivge@gmail.com>
2019-02-24 12:07:59 -0700

    Update FileReader.py

--- FileReader.py
+++ FileReader.py
@@ -5,11 +5,11 @@ import os
 
 open('config.txt', 'w')
 time.sleep(10)
-with open('config.txt') as f:
-    time.sleep(10)
-    f.write('+17202357772/n')
-    time.sleep(10)
-    f.write("destgps:(50,50)\n")
+f = open("config.txt", "a")
+time.sleep(10)
+f.write('+17202357772/n')
+time.sleep(10)
+f.write("destgps:(50,50)\n")
 
 while (True):
     my_file = Path("/home/linaro/Documents/TextMessage")
