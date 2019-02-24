19f602167c8fd0bc47b1905c9f698c3579c030bb
Zack Jorquera <jorquerazack@gmail.com>
2019-02-24 03:43:04 -0700

    Update gps_test.ino

--- gps_test.ino
+++ gps_test.ino
@@ -1,95 +1,49 @@
-/*
- Example code for connecting a Parallax GPS module to the Arduino
- Igor Gonzalez Martin. 05-04-2007
- igor.gonzalez.martin@gmail.com
- English translation by djmatic 19-05-2007
- Listen for the $GPRMC string and extract the GPS location data from this.
- Display the result in the Arduino's serial monitor.
- */ 
- #include <string.h>
- #include <ctype.h>
- int ledPin = 13;                  // LED test pin
- int rxPin = 0;                    // RX PIN 
- int txPin = 1;                    // TX TX
- int byteGPS=-1;
- char linea[300] = "";
- char comandoGPR[7] = "$GPRMC";
- int cont=0;
- int bien=0;
- int conta=0;
- int indices[13];
- void setup() {
-   pinMode(ledPin, OUTPUT);       // Initialize LED pin
-   pinMode(rxPin, INPUT);
-   pinMode(txPin, OUTPUT);
-   Serial.begin(4800);
-   for (int i=0;i<300;i++){       // Initialize a buffer for received data
-     linea[i]=' ';
-   }   
- }
- void loop() {
-   digitalWrite(ledPin, HIGH);
-   byteGPS=Serial.read();         // Read a byte of the serial port
-   if (byteGPS == -1) {           // See if the port is empty yet
-     delay(100); 
-   } else {
-     // note: there is a potential buffer overflow here!
-     linea[conta]=byteGPS;        // If there is serial port data, it is put in the buffer
-     conta++;                      
-     Serial.print(byteGPS, BYTE); 
-     if (byteGPS==13){            // If the received byte is = to 13, end of transmission
-       // note: the actual end of transmission is <CR><LF> (i.e. 0x13 0x10)
-       digitalWrite(ledPin, LOW); 
-       cont=0;
-       bien=0;
-       // The following for loop starts at 1, because this code is clowny and the first byte is the <LF> (0x10) from the previous transmission.
-       for (int i=1;i<7;i++){     // Verifies if the received command starts with $GPR
-         if (linea[i]==comandoGPR[i-1]){
-           bien++;
-         }
-       }
-       if(bien==6){               // If yes, continue and process the data
-         for (int i=0;i<300;i++){
-           if (linea[i]==','){    // check for the position of the  "," separator
-             // note: again, there is a potential buffer overflow here!
-             indices[cont]=i;
-             cont++;
-           }
-           if (linea[i]=='*'){    // ... and the "*"
-             indices[12]=i;
-             cont++;
-           }
-         }
-         Serial.println("");      // ... and write to the serial port
-         Serial.println("");
-         Serial.println("---------------");
-         for (int i=0;i<12;i++){
-           switch(i){
-             case 0 :Serial.print("Time in UTC (HhMmSs): ");break;
-             case 1 :Serial.print("Status (A=OK,V=KO): ");break;
-             case 2 :Serial.print("Latitude: ");break;
-             case 3 :Serial.print("Direction (N/S): ");break;
-             case 4 :Serial.print("Longitude: ");break;
-             case 5 :Serial.print("Direction (E/W): ");break;
-             case 6 :Serial.print("Velocity in knots: ");break;
-             case 7 :Serial.print("Heading in degrees: ");break;
-             case 8 :Serial.print("Date UTC (DdMmAa): ");break;
-             case 9 :Serial.print("Magnetic degrees: ");break;
-             case 10 :Serial.print("(E/W): ");break;
-             case 11 :Serial.print("Mode: ");break;
-             case 12 :Serial.print("Checksum: ");break;
-           }
-           for (int j=indices[i];j<(indices[i+1]-1);j++){
-             Serial.print(linea[j+1]); 
-           }
-           Serial.println("");
-         }
-         Serial.println("---------------");
-       }
-       conta=0;                    // Reset the buffer
-       for (int i=0;i<300;i++){    //  
-         linea[i]=' ';             
-       }                 
-     }
-   }
- }
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+/*This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.   
+It requires the use of SoftwareSerial, and assumes that you have a 9600-baud serial 
+GPS device hooked up on pins 8(rx) and 9(tx).*/
+static const int RXPin = 8, TXPin = 9;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+void setup()
+{
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+}
+
+void loop()
+{
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }  
+  Serial.println();
+}
