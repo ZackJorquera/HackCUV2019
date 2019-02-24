173d730e1919c7dfba104ef6d61fbeb084118668
Zack Jorquera <jorquerazack@gmail.com>
2019-02-23 21:23:22 -0700

    Delete sniff.py

--- sniff.py
+++
@@ -1,8 +0,0 @@
-import socket
-import struct
-import binascii
-
-s=socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800)) #socket class
-
-for x in range(1,10):
-    print(s.recvfrom(65565))
