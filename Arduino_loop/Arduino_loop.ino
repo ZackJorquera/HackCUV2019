c8da7ac10ca2c637a246dc4b6d9dbaf0862874e7
jorqueraian <jorqueraian@gmail.com>
2019-02-24 11:16:03 -0700

    Update Arduino_loop.ino

--- Arduino_loop/Arduino_loop.ino
+++ Arduino_loop/Arduino_loop.ino
@@ -1,44 +1,3 @@
-// Based on:
-// I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class using DMP (MotionApps v2.0)
-// 6/21/2012 by Jeff Rowberg <jeff@rowberg.net>
-// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
-//
-// Changelog:
-//      2013-05-08 - added seamless Fastwire support
-//                 - added note about gyro calibration
-//      2012-06-21 - added note about Arduino 1.0.1 + Leonardo compatibility error
-//      2012-06-20 - improved FIFO overflow handling and simplified read process
-//      2012-06-19 - completely rearranged DMP initialization code and simplification
-//      2012-06-13 - pull gyro and accel data from FIFO packet instead of reading directly
-//      2012-06-09 - fix broken FIFO read sequence and change interrupt detection to RISING
-//      2012-06-05 - add gravity-compensated initial reference frame acceleration output
-//                 - add 3D math helper file to DMP6 example sketch
-//                 - add Euler output and Yaw/Pitch/Roll output formats
-//      2012-06-04 - remove accel offset clearing for better results (thanks Sungon Lee)
-//      2012-06-01 - fixed gyro sensitivity to be 2000 deg/sec instead of 250
-//      2012-05-30 - basic DMP initialization working
-
-/* ============================================
-I2Cdev device library code is placed under the MIT license
-Copyright (c) 2012 Jeff Rowberg
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-===============================================
-*/
-
 //Writing to files
 #include <SPI.h>
 #include <SD.h>
@@ -46,9 +5,6 @@ THE SOFTWARE.
 //GPS Headers
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
-/*This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.   
-It requires the use of SoftwareSerial, and assumes that you have a 9600-baud serial 
-GPS device hooked up on pins 8(rx) and 9(tx).*/
 
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
@@ -70,64 +26,15 @@ GPS device hooked up on pins 8(rx) and 9(tx).*/
 MPU6050 mpu;
 //MPU6050 mpu(0x69); // <-- use for AD0 high
 
-/* =========================================================================
-   NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
-   depends on the MPU-6050's INT pin being connected to the Arduino's
-   external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
-   digital I/O pin 2.
- * ========================================================================= */
-
-/* =========================================================================
-   NOTE: Arduino v1.0.1 with the Leonardo board generates a compile error
-   when using Serial.write(buf, len). The Teapot output uses this method.
-   The solution requires a modification to the Arduino USBAPI.h file, which
-   is fortunately simple, but annoying. This will be fixed in the next IDE
-   release. For more info, see these links:
-   http://arduino.cc/forum/index.php/topic,109987.0.html
-   http://code.google.com/p/arduino/issues/detail?id=958
- * ========================================================================= */
-
-
-
-// uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
-// quaternion components in a [w, x, y, z] format (not best for parsing
-// on a remote host such as Processing or something though)
-//#define OUTPUT_READABLE_QUATERNION
-
-// uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
-// (in degrees) calculated from the quaternions coming from the FIFO.
-// Note that Euler angles suffer from gimbal lock (for more info, see
-// http://en.wikipedia.org/wiki/Gimbal_lock)
-//#define OUTPUT_READABLE_EULER
-
-// uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
-// pitch/roll angles (in degrees) calculated from the quaternions coming
-// from the FIFO. Note this also requires gravity vector calculations.
-// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
-// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
-//#define OUTPUT_READABLE_YAWPITCHROLL
-
-// uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
-// components with gravity removed. This acceleration reference frame is
-// not compensated for orientation, so +X is always +X according to the
-// sensor, just without the effects of gravity. If you want acceleration
-// compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
-//#define OUTPUT_READABLE_REALACCEL
-
 // uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
 // components with gravity removed and adjusted for the world frame of
 // reference (yaw is relative to initial orientation, since no magnetometer
 // is present in this case). Could be quite handy in some cases.
 #define OUTPUT_READABLE_WORLDACCEL//i think we want this one
 
-// uncomment "OUTPUT_TEAPOT" if you want output that matches the
-// format used for the InvenSense teapot demo
-//#define OUTPUT_TEAPOT
-
 
 
 #define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
-bool blinkState = false;
 
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
@@ -208,8 +115,8 @@ void setup()
     SD.begin(4);
 
     //GPS
-    //Serial.begin(115200);
-    //ss.begin(GPSBaud);
+    Serial.begin(115200);
+    ss.begin(GPSBaud);
    
     //Acceleration
     // join I2C bus (I2Cdev library doesn't do this automatically)
@@ -220,16 +127,9 @@ void setup()
         Fastwire::setup(400, true);
 #endif
 
-    // initialize serial communication
-    // (115200 chosen because it is required for Teapot Demo output, but it's
-    // really up to you depending on your project)
+   
     while (!Serial) ; // wait for Leonardo enumeration, others continue immediately
 
-    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
-    // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
-    // the baud timing being too misaligned with processor ticks. You must use
-    // 38400 or slower in these cases, or use some kind of external separate
-    // crystal solution for the UART timer.
 
     // initialize device
     Serial.println(F("Initializing I2C devices..."));
@@ -249,11 +149,7 @@ void setup()
     Serial.println(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
 
-    // supply your own gyro offsets here, scaled for min sensitivity
-    //mpu.setXGyroOffset(220);
-    //mpu.setYGyroOffset(76);
-    //mpu.setZGyroOffset(-85);
-    //mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+    
 
     // make sure it worked (returns 0 if so)
     if (devStatus == 0)
@@ -310,15 +206,7 @@ void loop()
     while (!mpuInterrupt && fifoCount < packetSize)
     {
         // other program behavior stuff here
-        // .
-        // .
-        // .
-        // if you are really paranoid you can frequently test in between other
-        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-        // while() loop to immediately process the MPU data
-        // .
-        // .
-        // .
+
     }
 
     // reset interrupt flag and get INT_STATUS byte
@@ -476,9 +364,7 @@ void loop()
         //teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
 //#endif
 
-        // blink LED to indicate activity
-        blinkState = !blinkState;
-        digitalWrite(LED_PIN, blinkState);
+
     }
 
     if (iterator % 50 == 0)
@@ -494,23 +380,6 @@ void loop()
         while (ss.available() > 0)
             if (gps.encode(ss.read()))
                 displayInfo();
-
-        //if (millis() > 5000 && gps.charsProcessed() < 10)
-        //{
-        //  Serial.println(F("No GPS detected: check wiring."));
-        //  while(true);
-        //}
-
-        //check thresholds
-       // if (water_reading > water_sensor_threshold)
-        //{
-//          char c[100] = "Possible water damage! water level: ";
-//          sprintf(c, "%f", water_reading);
-//          text_load("msg", c);
-//        }
-
-        //Get Data
-        //text_load();
     }
 
     text_send();
@@ -523,49 +392,22 @@ void text_load(String tag, String data)
     text_msg += (tag + ":" + data + "\n");
 }
 
-//void uploadScript() 
-//{
-//  File script = FileSystem.open("/tmp/~/TextMessage", FILE_WRITE);
-//  script.print(text_msg);
-//  script.close();
-//}
-
 void text_send()
 {
   dataFile = SD.open("TextMessage.txt", FILE_WRITE);
 
   dataFile.print(text_msg);
-
-  
-    //call python file
-    //uncomment when python script is present
-    
-      //uploadScript();
-     //TODO pass text_msg to txt file
-
-     //output.print( text_msg );
     text_msg = "";
 }
 
 void displayInfo()
 {
-  if (gps.location.isValid())
-  {
-    Serial.print(F("Location: "));
+   Serial.print(F("Location: "));
     Serial.print(gps.location.lat(), 6);
     Serial.print(F(","));
-    Serial.print(gps.location.lng(), 6);
+    Serial.println(gps.location.lng(), 6);
      
     char c[200] = "";
     sprintf(c, "(%f,%f)", gps.location.lat(), gps.location.lng());
     text_load("gps", c);
-  }
-  //else
-  //{
-  //  Serial.print(F("INVALID"));
-  //  Serial.print("40.0096", 6);
-  //  Serial.print(F(","));
-  //  Serial.print("-105.2419", 6);
-  //}  
-  Serial.println();
 }
