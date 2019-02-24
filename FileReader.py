7c8359929d6f8b8e96317db7f8777f2518a8d650
Borab3 <nadivge@gmail.com>
2019-02-24 11:56:52 -0700

    Update FileReader.py

--- FileReader.py
+++ FileReader.py
@@ -3,11 +3,12 @@ from pathlib2 import Path
 import time
 import os
 
-f = open('config.txt', 'w')
-
-f.write('+17202357772')
-f.write("destgps:(50,50)\n")
-f.close()
+open('config.txt', 'w').close()
+with open('config.txt') as f:
+    f.write('+17202357772')
+    fx
+    f.write("destgps:(50,50)\n")
+    f.close()
 
 initialise = True
 
@@ -16,15 +17,6 @@ while (True):
     if my_file.is_file():
         content = my_file.read_text()
         os.remove(str(my_file))
-
-        for line in content.split('\n'):
-            if (initialise == True):
-                f = open('config.txt', 'r')
-                (f.readline())
-                line = (f.readline()) + line
-                f.close()
-                initialise = False
-            print("sending: ", line)
-            sendText.text(str(line))
+        sendText.text(str(content))
 
     time.sleep(10)
