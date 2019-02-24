2949a073e8a284590859cd07958c66b59d9d2918
Borab3 <nadivge@gmail.com>
2019-02-24 10:33:08 -0700

    Update FileReader.py

--- FileReader.py
+++ FileReader.py
@@ -3,14 +3,28 @@ from pathlib2 import Path
 import time
 import os
 
-while(True):
-    my_file = Path("/tmp/~/TextMessage")
+os.mknod('config.txt')
+with open('config.txt') as f:
+    f.write('+17202357772')
+    f.write("destgps:(50,50)\n")
+    f.close()
+
+initialise = True
+
+while (True):
+    my_file = Path("/home/linaro/Documents/TextMessage")
     if my_file.is_file():
         content = my_file.read_text()
         os.remove(str(my_file))
-        
+
         for line in content.split('\n'):
+            if (initialise = True):
+                f.open('config.txt', 'r')
+                (f.readline())
+                line = print(f.readline()) + line
+                f.close()
+                initialise = False
             print("sending: ", line)
             sendText.text(str(line))
-    
+
     time.sleep(10)
