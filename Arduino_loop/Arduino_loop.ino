16d9877816c5e871cca22754dc4594a439958699
jorqueraian <jorqueraian@gmail.com>
2019-02-24 04:50:06 -0700

    Update Arduino_loop.ino

--- Arduino_loop/Arduino_loop.ino
+++ Arduino_loop/Arduino_loop.ino
@@ -166,6 +166,63 @@ void dmpDataReady() {
 // ===                      INITIAL SETUP                       ===
 // ================================================================
 
+
+/*
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+/*This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.   
+It requires the use of SoftwareSerial, and assumes that you have a 9600-baud serial 
+GPS device hooked up on pins 8(rx) and 9(tx).*/
+static const int RXPin = 8, TXPin = 9;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+void setup()
+{
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+}
+
+void loop()
+{
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  //else
+  //{
+  //  Serial.print(F("INVALID"));
+  //  Serial.print("40.0096", 6);
+  //  Serial.print(F(","));
+  //  Serial.print("-105.2419", 6);
+  //}  
+  Serial.println();
+}
+
+*/
+
 void setup() 
 {
     //water sensor
