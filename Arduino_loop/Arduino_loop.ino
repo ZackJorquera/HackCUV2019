88202486ebd1a668dce2a8c650e010fcfbdab0f1
jorqueraian <jorqueraian@gmail.com>
2019-02-24 05:06:05 -0700

    Update Arduino_loop.ino

--- Arduino_loop/Arduino_loop.ino
+++ Arduino_loop/Arduino_loop.ino
@@ -38,17 +38,24 @@ THE SOFTWARE.
 ===============================================
 */
 
+//GPS Headers
+# include <TinyGPS++.h>
+# include <SoftwareSerial.h>
+/*This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.   
+It requires the use of SoftwareSerial, and assumes that you have a 9600-baud serial 
+GPS device hooked up on pins 8(rx) and 9(tx).*/
+
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
-#include "I2Cdev.h"
+# include "I2Cdev.h"
 
-#include "MPU6050_6Axis_MotionApps20.h"
+# include "MPU6050_6Axis_MotionApps20.h"
 //#include "MPU6050.h" // not necessary if using MotionApps include file
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    #include "Wire.h"
+# include "Wire.h"
 #endif
 
 // class default I2C address is 0x68
@@ -136,27 +143,40 @@ float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 // packet structure for InvenSense teapot demo
-uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
 
 
 //Thresholds
 float jerk_thershold = 8;
-float water_threshold = 0;
+float water_threshold = 80;
 
 //functionsto load text
 String text_msg = "";
 
 void text_load(String tag, String data);
 void text_send();
+void displayInfo();
 
 int iterator = 0;
+
+//GPS constants
+static const int RXPin = 8, TXPin = 9;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
 
 // ================================================================
 // ===               INTERRUPT DETECTION ROUTINE                ===
 // ================================================================
 
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
+void dmpDataReady()
+{
     mpuInterrupt = true;
 }
 
@@ -167,84 +187,30 @@ void dmpDataReady() {
 // ================================================================
 
 
-/*
-#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
-/*This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.   
-It requires the use of SoftwareSerial, and assumes that you have a 9600-baud serial 
-GPS device hooked up on pins 8(rx) and 9(tx).*/
-static const int RXPin = 8, TXPin = 9;
-static const uint32_t GPSBaud = 9600;
-
-// The TinyGPS++ object
-TinyGPSPlus gps;
-
-// The serial connection to the GPS device
-SoftwareSerial ss(RXPin, TXPin);
-
+]
 void setup()
-{
-  Serial.begin(115200);
-  ss.begin(GPSBaud);
-}
-
-void loop()
-{
-  // This sketch displays information every time a new sentence is correctly encoded.
-  while (ss.available() > 0)
-    if (gps.encode(ss.read()))
-      displayInfo();
-
-  if (millis() > 5000 && gps.charsProcessed() < 10)
-  {
-    Serial.println(F("No GPS detected: check wiring."));
-    while(true);
-  }
-}
-
-void displayInfo()
-{
-  Serial.print(F("Location: ")); 
-  if (gps.location.isValid())
-  {
-    Serial.print(gps.location.lat(), 6);
-    Serial.print(F(","));
-    Serial.print(gps.location.lng(), 6);
-  }
-  //else
-  //{
-  //  Serial.print(F("INVALID"));
-  //  Serial.print("40.0096", 6);
-  //  Serial.print(F(","));
-  //  Serial.print("-105.2419", 6);
-  //}  
-  Serial.println();
-}
-
-*/
-
-void setup() 
 {
     //water sensor
     Serial.begin(9600);     // Communication started with 9600 baud
-    
+
     //GPS
-    
+    Serial.begin(115200);
+    ss.begin(GPSBaud);
     //Accel
-    
+
     // join I2C bus (I2Cdev library doesn't do this automatically)
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Wire.begin();
-        TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
         Fastwire::setup(400, true);
-    #endif
+#endif
 
     // initialize serial communication
     // (115200 chosen because it is required for Teapot Demo output, but it's
     // really up to you depending on your project)
     Serial.begin(115200);
-    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+    while (!Serial) ; // wait for Leonardo enumeration, others continue immediately
 
     // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
     // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
@@ -262,9 +228,9 @@ void setup()
 
     // wait for ready
     Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    while (Serial.available() && Serial.read()); // empty buffer
-    while (!Serial.available());                 // wait for data
-    while (Serial.available() && Serial.read()); // empty buffer again
+    while (Serial.available() && Serial.read()) ; // empty buffer
+    while (!Serial.available()) ;                 // wait for data
+    while (Serial.available() && Serial.read()) ; // empty buffer again
 
     // load and configure the DMP
     Serial.println(F("Initializing DMP..."));
@@ -277,7 +243,8 @@ void setup()
     //mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
 
     // make sure it worked (returns 0 if so)
-    if (devStatus == 0) {
+    if (devStatus == 0)
+    {
         // turn on the DMP, now that it's ready
         Serial.println(F("Enabling DMP..."));
         mpu.setDMPEnabled(true);
@@ -293,7 +260,9 @@ void setup()
 
         // get expected DMP packet size for later comparison
         packetSize = mpu.dmpGetFIFOPacketSize();
-    } else {
+    }
+    else
+    {
         // ERROR!
         // 1 = initial memory load failed
         // 2 = DMP configuration updates failed
@@ -315,12 +284,14 @@ void setup()
 // ===                    MAIN PROGRAM LOOP                     ===
 // ================================================================
 
-void loop() {
+void loop()
+{
     // if programming failed, don't try to do anything
     if (!dmpReady) return;
 
     // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt && fifoCount < packetSize) {
+    while (!mpuInterrupt && fifoCount < packetSize)
+    {
         // other program behavior stuff here
         // .
         // .
@@ -341,128 +312,131 @@ void loop() {
     fifoCount = mpu.getFIFOCount();
 
     // check for overflow (this should never happen unless our code is too inefficient)
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024)
+    {
         // reset so we can continue cleanly
         mpu.resetFIFO();
         Serial.println(F("FIFO overflow!"));
 
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    } else if (mpuIntStatus & 0x02) {
+        // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    }
+    else if (mpuIntStatus & 0x02)
+    {
         // wait for correct available data length, should be a VERY short wait
         while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
 
         // read a packet from FIFO
         mpu.getFIFOBytes(fifoBuffer, packetSize);
-        
+
         // track FIFO count here in case there is > 1 packet available
         // (this lets us immediately read more without waiting for an interrupt)
         fifoCount -= packetSize;
 
-        #ifdef OUTPUT_READABLE_QUATERNION
-            // display quaternion values in easy matrix form: w x y z
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            Serial.print("quat\t");
-            Serial.print(q.w);
-            Serial.print("\t");
-            Serial.print(q.x);
-            Serial.print("\t");
-            Serial.print(q.y);
-            Serial.print("\t");
-            Serial.println(q.z);
-        #endif
+# ifdef OUTPUT_READABLE_QUATERNION
+        // display quaternion values in easy matrix form: w x y z
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        Serial.print("quat\t");
+        Serial.print(q.w);
+        Serial.print("\t");
+        Serial.print(q.x);
+        Serial.print("\t");
+        Serial.print(q.y);
+        Serial.print("\t");
+        Serial.println(q.z);
+#endif
 
-        #ifdef OUTPUT_READABLE_EULER
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetEuler(euler, &q);
-            Serial.print("euler\t");
-            Serial.print(euler[0] * 180/M_PI);
-            Serial.print("\t");
-            Serial.print(euler[1] * 180/M_PI);
-            Serial.print("\t");
-            Serial.println(euler[2] * 180/M_PI);
-        #endif
+# ifdef OUTPUT_READABLE_EULER
+        // display Euler angles in degrees
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetEuler(euler, &q);
+        Serial.print("euler\t");
+        Serial.print(euler[0] * 180 / M_PI);
+        Serial.print("\t");
+        Serial.print(euler[1] * 180 / M_PI);
+        Serial.print("\t");
+        Serial.println(euler[2] * 180 / M_PI);
+#endif
 
-        #ifdef OUTPUT_READABLE_YAWPITCHROLL
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-            Serial.print("ypr\t");
-            Serial.print(ypr[0] * 180/M_PI);
-            Serial.print("\t");
-            Serial.print(ypr[1] * 180/M_PI);
-            Serial.print("\t");
-            Serial.println(ypr[2] * 180/M_PI);
-        #endif
+# ifdef OUTPUT_READABLE_YAWPITCHROLL
+        // display Euler angles in degrees
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+        Serial.print("ypr\t");
+        Serial.print(ypr[0] * 180 / M_PI);
+        Serial.print("\t");
+        Serial.print(ypr[1] * 180 / M_PI);
+        Serial.print("\t");
+        Serial.println(ypr[2] * 180 / M_PI);
+#endif
 
-//THis is all we care about!!!
-        #ifdef OUTPUT_READABLE_REALACCEL
-            // display real acceleration, adjusted to remove gravity
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            Serial.print("areal\t");
-            Serial.print(aaReal.x);
-            Serial.print("\t");
-            Serial.print(aaReal.y);
-            Serial.print("\t");
-            Serial.println(aaReal.z);
-            
-            //Logic for dropage
-            if (aaReal_p.x != -100)
+        //THis is all we care about!!!
+# ifdef OUTPUT_READABLE_REALACCEL
+        // display real acceleration, adjusted to remove gravity
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetAccel(&aa, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+        Serial.print("areal\t");
+        Serial.print(aaReal.x);
+        Serial.print("\t");
+        Serial.print(aaReal.y);
+        Serial.print("\t");
+        Serial.println(aaReal.z);
+
+        //Logic for dropage
+        if (aaReal_p.x != -100)
+        {
+            //jerk calculations. if Jerk > 8.
+            //magnitude of the jerk. in this cae direction doesnt matter
+            //also for fast computation we only care about the jerk^2
+
+            float jerk_mag_squared = sq(3 * (aaReal.x - aaReal_p.x)) + sq(3 * (aaReal.y - aaReal_p.y)) + sq(3 * (aaReal.z - aaReal_p.z));
+
+
+            if (jerk_mag_squared > sq(jerk_thershold))
             {
-              //jerk calculations. if Jerk > 8.
-              //magnitude of the jerk. in this cae direction doesnt matter
-              //also for fast computation we only care about the jerk^2
-
-              float jerk_mag_squared = sq(3*(aaReal.x - aaReal_p.x)) + sq(3*(aaReal.y - aaReal_p.y)) + sq(3*(aaReal.z - aaReal_p.z));
-              
-
-              if (jerk_mag_squared > sq(jerk_thershold))
-              {
                 char c[100] = "Possible drop: Jerk^2(>8): ";
                 sprintf(c, "%f", jerk_mag_squared);
                 text_load("msg", c);
-              }
             }
-            //set prev values at end of loop, init at -100;
-            aaReal_p.x = aaReal.x;
-            aaReal_p.x = aaReal.x;
-            aaReal_p.x = aaReal.x;
-        #endif
+        }
+        //set prev values at end of loop, init at -100;
+        aaReal_p.x = aaReal.x;
+        aaReal_p.x = aaReal.x;
+        aaReal_p.x = aaReal.x;
+#endif
 
-        #ifdef OUTPUT_READABLE_WORLDACCEL
-            // display initial world-frame acceleration, adjusted to remove gravity
-            // and rotated based on known orientation from quaternion
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-            Serial.print("aworld\t");
-            Serial.print(aaWorld.x);
-            Serial.print("\t");
-            Serial.print(aaWorld.y);
-            Serial.print("\t");
-            Serial.println(aaWorld.z);
-            
-        #endif
-    
-        #ifdef OUTPUT_TEAPOT
-            // display quaternion values in InvenSense Teapot demo format:
-            teapotPacket[2] = fifoBuffer[0];
-            teapotPacket[3] = fifoBuffer[1];
-            teapotPacket[4] = fifoBuffer[4];
-            teapotPacket[5] = fifoBuffer[5];
-            teapotPacket[6] = fifoBuffer[8];
-            teapotPacket[7] = fifoBuffer[9];
-            teapotPacket[8] = fifoBuffer[12];
-            teapotPacket[9] = fifoBuffer[13];
-            Serial.write(teapotPacket, 14);
-            teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
-        #endif
+# ifdef OUTPUT_READABLE_WORLDACCEL
+        // display initial world-frame acceleration, adjusted to remove gravity
+        // and rotated based on known orientation from quaternion
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetAccel(&aa, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+        mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+        Serial.print("aworld\t");
+        Serial.print(aaWorld.x);
+        Serial.print("\t");
+        Serial.print(aaWorld.y);
+        Serial.print("\t");
+        Serial.println(aaWorld.z);
+
+#endif
+
+# ifdef OUTPUT_TEAPOT
+        // display quaternion values in InvenSense Teapot demo format:
+        teapotPacket[2] = fifoBuffer[0];
+        teapotPacket[3] = fifoBuffer[1];
+        teapotPacket[4] = fifoBuffer[4];
+        teapotPacket[5] = fifoBuffer[5];
+        teapotPacket[6] = fifoBuffer[8];
+        teapotPacket[7] = fifoBuffer[9];
+        teapotPacket[8] = fifoBuffer[12];
+        teapotPacket[9] = fifoBuffer[13];
+        Serial.write(teapotPacket, 14);
+        teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
+#endif
 
         // blink LED to indicate activity
         blinkState = !blinkState;
@@ -471,16 +445,29 @@ void loop() {
 
     if (iterator % 500 == 0)
     {
-      //Other codes: GPS, Water
+        //Other codes: GPS, Water
         int water_reading = analogRead(A1); // Incoming analog signal read and appointed sensor
-        
+
+        //GPS
+        // This sketch displays information every time a new sentence is correctly encoded.
+        while (ss.available() > 0)
+            if (gps.encode(ss.read()))
+                displayInfo();
+
+        //if (millis() > 5000 && gps.charsProcessed() < 10)
+        //{
+        //  Serial.println(F("No GPS detected: check wiring."));
+        //  while(true);
+        //}
+
+
         if (water_reading > water_sensor_threshold)
         {
             //text_load()
         }
 
-      //Get Data
-      //text_load();
+        //Get Data
+        //text_load();
     }
 
     text_send();
@@ -489,14 +476,33 @@ void loop() {
 
 void text_load(String tag, String data)
 {
-  text_msg += (tag + ":" + data + "\n"); 
+    text_msg += (tag + ":" + data + "\n");
 }
 
 void text_send()
 {
-  //call python file
-  //uncomment when python script is present
-  //p.runShellCommand("python ~/sendText.py \"" + text_msg + "\"" );// wont work unless fine is there
-  
-  text_msg = "";
+    //call python file
+    //uncomment when python script is present
+    //p.runShellCommand("python ~/sendText.py \"" + text_msg + "\"" );// wont work unless fine is there
+
+    text_msg = "";
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  //else
+  //{
+  //  Serial.print(F("INVALID"));
+  //  Serial.print("40.0096", 6);
+  //  Serial.print(F(","));
+  //  Serial.print("-105.2419", 6);
+  //}  
+  Serial.println();
 }
