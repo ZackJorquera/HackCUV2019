8d2228a542c3ab4093b12b1b19a6bb9beb933352
Zack Jorquera <jorquerazack@gmail.com>
2019-02-24 02:17:06 -0700

    Update example_accel.ino

--- example_accel.ino
+++ example_accel.ino
@@ -130,7 +130,7 @@ uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 
 // orientation/motion vars
-//Quaternion q;           // [w, x, y, z]         quaternion container
+Quaternion q;           // [w, x, y, z]         quaternion container
 VectorInt16 aa;         // [x, y, z]            accel sensor measurements
 VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
 VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
